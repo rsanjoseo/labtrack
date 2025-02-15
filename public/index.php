0cf866f40536dcddd317c74e5e9f88cf55fbe4ff
Rafael San José Tovar <rsanjose@alxarafe.com>
2025-02-15 19:13:42 +0100

    The main file public/index.php file is added.

---
+++ public/index.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Copyright (C) 2025       Rafael San José         <rsanjose@alxarafe.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use Alxarafe\Lib\Functions;
+use Alxarafe\Tools\Dispatcher;
+
+const BASE_PATH = __DIR__;
+
+$autoload_filename = realpath(BASE_PATH . '/../vendor/autoload.php');
+if (!file_exists($autoload_filename)) {
+    die('<h1>COMPOSER ERROR</h1><p>You need to run: "composer install"</p>');
+}
+
+require_once $autoload_filename;
+
+define('BASE_URL', Functions::getUrl());
+
+try {
+    Dispatcher::run();
+} catch (\DebugBar\DebugBarException $e) {
+    dd($e);
+}
